843e508d9c7a7e1b12f24489e7f7118d28706763
Kevin Stadler <kevinstadler@users.noreply.github.com>
2026-01-21 11:30:01 +0100

    improvement: also support %.* chaining in filters

--- src/wisskas/filter.py
+++ src/wisskas/filter.py
@@ -266,7 +266,7 @@ def clone_include(
             name: clone_include(
                 clone,
                 name,
-                includes.get(name, includes.get("*", [])),
+                includes.get(name, includes.get("*", includes.get("%", []))),
                 prefix,
                 used_names,
                 depth + 1,
@@ -277,6 +277,7 @@ def clone_include(
             or "%" in include
             or name in includes
             or any(i.startswith("*.") for i in include)
+            or any(i.startswith("%.") for i in include)
         }
     if len(clone.fields) == 0 and not clone.datatype_property and not clone.type:
         debug_filter(clone, "class is down to 0 fields", depth)
